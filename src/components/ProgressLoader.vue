663bf17321d34a7a9ec52db491c85175515289bf
Rawbee <bhinxy18@yahoo.com>
2020-04-24 14:12:17 +1000

    Convert progress spinner to use CSS so it's easy to integrate with buttons

--- src/components/ProgressLoader.vue
+++ src/components/ProgressLoader.vue
@@ -1,19 +1,5 @@
 <template>
-  <div class="progress-loader">
-    <svg
-      class="progress-loader__spinner progress-loader--primary"
-      viewBox="0 0 50 50"
-    >
-      <circle
-        class="path"
-        cx="25"
-        cy="25"
-        r="20"
-        fill="none"
-        stroke-width="5"
-      ></circle>
-    </svg>
-  </div>
+  <div class="progress-loader"/>
 </template>
 
 <script lang="ts">
@@ -26,55 +12,41 @@ export default class ProgressLoader extends Vue {
 </script>
 
 <style lang="scss" scoped>
-/** https://material.io/resources/color/#!/?view.left=0&view.right=0&secondary.color=eaeaea&primary.color=7B1FA2 */
-$color-primary: #7b1fa2;
-$color-primary--light: #ae52d4;
-$color-primary--dark: #4a0072;
-$color-secondary: #eaeaea;
-$color-secondary--light: #ffffff;
-$color-secondary--dark: #b8b8b8;
+.progress-loader {
+  border: 10px solid $color-primary--light;
+  border-top: 10px solid $color-secondary;
+  border-radius: 50%;
+  max-width: 50px;
+  max-height: 50px;
+  -webkit-animation: spin 1s linear infinite; /* Safari */
+  animation: spin 1s linear infinite;
+  display: inline-block;
+  margin-right: 5px;
+}
 
-.progress-loader__spinner {
-  animation: rotate 2s linear infinite;
-  z-index: 2;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin: -25px 0 0 -25px;
-  width: 50px;
-  height: 50px;
+.progress-loader--small {
+  width: 16px;
+  height: 16px;
+  border: 5px solid $color-primary--light;
+  border-top: 5px solid $color-secondary;
+}
 
-  & .path {
-    stroke: hsl(210, 70, 75);
-    stroke-linecap: round;
-    animation: dash 1.5s ease-in-out infinite;
+/* Safari */
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
   }
 }
 
-.progress-loader--primary {
-  & .path {
-    stroke: $color-primary;
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
   }
-}
-
-@keyframes rotate {
   100% {
     transform: rotate(360deg);
-  }
-}
-
-@keyframes dash {
-  0% {
-    stroke-dasharray: 1, 150;
-    stroke-dashoffset: 0;
-  }
-  50% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -35;
-  }
-  100% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -124;
   }
 }
 </style>
--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="components">
-    <ProgressLoader />
+    <button class="btn btn--default txt--center" @click="showSpinner">
+      <ProgressLoader v-if="isSpinnerVisible" class="progress-loader--small"/>&nbsp;Submit
+    </button>
   </div>
 </template>
 
-<script>
+<style lang="scss" scoped>
+.btn {
+  border: 2px solid black;
+  background-color: white;
+  color: black;
+  padding: 14px;
+  font-size: 16px;
+  cursor: pointer;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.txt--center {
+  margin: 0 auto;
+}
+
+/* Green */
+.btn--success {
+  border-color: #4caf50;
+  color: green;
+
+  &:hover {
+    background-color: #4caf50;
+    color: white;
+  }
+}
+
+/* Gray */
+.btn--default {
+  border-color: #e7e7e7;
+  color: black;
+  &:hover {
+    background: #e7e7e7;
+  }
+}
+</style>
+
+<script lang="ts">
+import { Component, Emit, Vue } from "vue-property-decorator";
 import ProgressLoader from "@/components/ProgressLoader.vue";
 
-export default {
-  name: "ComponentsPage",
-  components: {
-    ProgressLoader
+@Component({
+  components: { ProgressLoader }
+})
+export default class Components extends Vue {
+  isSpinnerVisible = false;
+
+  @Emit()
+  showSpinner() {
+    this.isSpinnerVisible = true;
   }
-};
+}
 </script>
